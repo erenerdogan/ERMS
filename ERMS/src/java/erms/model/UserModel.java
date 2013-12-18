bdf121f004b26d5c724494d24b8cd2104c21a35f
Eren <eren@Eren-MacBook-Air.local>
2013-12-18 18:41:08 +0200

    Projeye Başlandı.
    Elektronik Restaurant Menu Sistemi için geliştirmeye başlandı.

---
+++ ERMS/src/java/erms/model/UserModel.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package erms.model;
+
+/**
+ *
+ * @author eren
+ */
+public class UserModel {
+    
+}
